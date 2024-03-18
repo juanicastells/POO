e79eef342e9c52562fbe8662e206263f197d5686
juanicastells <juan-c65@live.com.ar>
2024-03-18 10:16:07 -0300

    configuracion de logout

---
+++ proyectoConcurso/demo/src/main/java/com/example/demo/business/MaterialEducativoBusiness.java
@@ -0,0 +1,33 @@
+package com.example.demo.business;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.MaterialEducativo;
+import com.example.demo.entity.Usuario;
+import com.example.demo.repository.MaterialEducativoRepository;
+
+@Service
+public class MaterialEducativoBusiness {
+    
+    @Autowired
+    MaterialEducativoRepository materialEducativoRepository;
+
+    public MaterialEducativo crearNuevoMaterial (/*Usuario autorMaterial*/){
+        MaterialEducativo nuevoMaterial = new MaterialEducativo();
+        //autorMaterial.setMaterialEducativo(nuevoMaterial);
+        return materialEducativoRepository.save(nuevoMaterial);
+    }
+
+    public MaterialEducativo actualizarMaterialEducativo (Long idMaterial, String nuevoNombre, String nuevaDescripcion) /*MultipartFile documento)*/{
+        MaterialEducativo materialEducativo = materialEducativoRepository.findById(idMaterial).orElse(null);
+        if (materialEducativo != null) {
+            materialEducativo.setNombre(nuevoNombre);
+            materialEducativo.setDescripcion(nuevaDescripcion);
+            //materialEducativo.setArchivoMaterial(documento);
+            return materialEducativoRepository.save(materialEducativo);
+        } else {
+            return null;
+        }
+    }
+}
---
+++ proyectoConcurso/demo/src/main/java/com/example/demo/controller/AuthConcursanteController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.business.CustomUserDetailsBusiness;
+
+@Controller
+public class AuthConcursanteController {
+    
+    @Autowired
+    private CustomUserDetailsBusiness customUserDetailsBusiness;
+
+    
+    @GetMapping("/")
+    public String principal(){
+        return "loginConcursante";
+    }
+    
+    @GetMapping("/loginConcursante")
+    public String loginConcursante(){
+        return "loginConcursante";
+    }
+
+    @GetMapping("/inicioConcursante")
+    public String inicioConcursante(){
+        return "inicioConcursante";
+    }
+
+}
